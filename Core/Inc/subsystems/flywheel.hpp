3ca4b86cfa626650aff2de8c32f364c5702a56a3
Sid Prabhakaran <s2siddhu@gmail.com>
2020-09-18 18:47:08 -0700

    Updated flywheel.hpp/cpp for Flywheel Team
    - Flywheel team can finally do their task now
    
    9/10/20

--- Core/Inc/subsystems/flywheel.hpp
+++ Core/Inc/subsystems/flywheel.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
 #include "cmsis_os.h"
+#include "information/device.hpp"
+#include "information/pid.hpp"
+#include "stm32f4xx_hal.h"
 
 namespace flywheel {
 
-extern float f1Power;
-extern float f2Power;
+// class flywheelMotor : public pwmMotor {
+// private:
+//     pidInstance* PID;
+
+// public:
+//     flywheelMotor(TIM_HandleTypeDef* tim, int tim_channel, GPIO_TypeDef* GPIOx, int GPIO_Pin, int lR, int uR, float32_t lC, float32_t uC) : pwmMotor(tim, tim_channel, GPIOx, GPIO_Pin, lR, uR, lC, uC);
+//     flywheelMotor(TIM_HandleTypeDef* tim, int tim_channel, GPIO_TypeDef* GPIOx, int GPIO_Pin, int lR, int uR) : pwmMotor(tim, tim_channel, GPIOx, GPIO_Pin, lR, uR, -100, 100);
+//     flywheelMotor(TIM_HandleTypeDef* tim, int tim_channel, GPIO_TypeDef* GPIOx, int GPIO_Pin) : pwmMotor(tim, tim_channel, GPIOx, GPIO_Pin, 533, 1083, -100, 100);
+// };
 
 enum flywheelStates {
-    notRunning = 'x',
-    running = 'r'
+    notRunning,
+    running
 };
 extern flywheelStates currState;
 
@@ -18,5 +28,7 @@ extern void task();
 extern void update();
 
 extern void act();
+
+extern double calcSlewDRpm(double currFw1Speed, double currFw2Speed, double targetFw1Speed, double targetFw2Speed);
 
 } // namespace flywheel
--- Core/Src/subsystems/flywheel.cpp
+++ Core/Src/subsystems/flywheel.cpp
@@ -5,12 +5,12 @@ namespace flywheel {
 
 flywheelStates currState = notRunning;
 
-float f1Power = 0;
-float f2Power = 0;
+double angularAccLimit = 0.1; // defined as rpm/10ms^2
+
+// flywheelMotor flyhweel1(&htim2, 1, POWER1_CTRL_GPIO_Port, POWER1_CTRL_Pin);
 
 void task() {
 
-    
     for (;;) {
         update();
 
@@ -31,14 +31,34 @@ void update() {
 void act() {
     switch (currState) {
     case notRunning:
-        f1Power = f2Power = 0;
         break;
 
     case running:
-        f1Power = f2Power = 0;
         // obviously this will change when we have actual intelligent things to put here
         break;
     }
+}
+
+double calcSlewDRpm(double currFw1Speed, double currFw2Speed, double targetFw1Speed, double targetFw2Speed) {
+    // IMPLEMENT THIS
+    /* 
+    Given the current speeds (rpm) of both flywheel motors, a target speed, and an angular acceleration cap
+    (globally defined as angularAccLimit on line 8)
+        - Calculate the largest amount the flywheel speed should change by while still
+        adhering to the acceleration constraint.
+        - For now, set your calculated values to fw1DRPM and fw2DRPM.
+
+    EX: If the current speed of the flywheels are 50rpm, and the target speed given is 80rpm, calculate what
+    speed the flywheels should be set to THIS LOOP while adhering to the acceleration limit.
+
+    NOTE: As I write this, I'm realizing this task might be dummy simple. If that's the case, go ahead and have
+    some fun with it, whether that means adding some more kinematic constraints, or allowing us to set a current
+    cap rather than an acceleration cap; idc the sky's the limit. I'll get better at writing these as time goes :/
+    */
+
+    double fw1DRPM;
+    double fw2DRPM;
+    return 0;
 }
 
 } // namespace flywheel
