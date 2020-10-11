53d8a395490f5fcb6055f10c840f5df3c5da662d
evan-krohn <evanpk@yahoo.com>
2020-10-11 03:39:19 -0500

    Add .gitignore

--- MDK-ARM/DevACustom/retarget_io.d
+++
@@ -1,8 +0,0 @@
-devacustom\retarget_io.o: C:\Users\s2sid\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.6.3\Source\retarget_io.c
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\assert.h
-devacustom\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_sys.h
-devacustom\retarget_io.o: .\RTE\_DevACustom\RTE_Components.h
