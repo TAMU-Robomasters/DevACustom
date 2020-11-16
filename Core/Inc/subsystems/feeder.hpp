39f71ce88c7593ec7308dcfbaa5fd855756fdce0
GauravBhalla990 <gauravbhalla990gmail.com>
2020-11-15 21:49:18 -0600

    Added feederMotor class in feeder.hpp

--- Core/Inc/subsystems/feeder.hpp
+++ Core/Inc/subsystems/feeder.hpp
@@ -1,7 +1,40 @@
 #pragma once
+// header guard
 
 #include "cmsis_os.h"
 //#include "information/can_protocol.hpp"
+#include "information/device.hpp"
+#include "information/pid.hpp"
+#include <math.h>
+
+
+#define rotor_angle_max 8091;
+#define radsPerTick (2 * PI) / rotor_angle_max;
+
+class feederMotor: public canMotor{
+	private:
+		pidInstance* PID;
+		double currAngle; // in radians
+	public:
+		feederMotor(int16_t ID, float32_t lC, float32_t uC): canMotor(ID, -100, 100){}
+		feederMotor(int16_t ID, float32_t lC, float32_t uC, pidInstance& pid) : canMotor(ID, -100, 100), PID(&pid) {}
+		feederMotor(int16_t ID, pidInstance& pid) : canMotor(ID, -100, 100), PID(&pid) {}
+
+		void setPID(pidInstance& pid) {
+		        PID = &pid;
+		}
+
+		double getCurrAngle() {
+			return currAngle;
+		}
+
+        void setCurrAngle(double ticks) {
+        	currAngle = ticks * radsPerTick;
+		}
+
+};
+
+
 
 namespace feeder {
 
