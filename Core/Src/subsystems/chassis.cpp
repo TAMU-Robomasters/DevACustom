83cccccce15235bda68d984395b24e2a8a9434b5
Prabhakaran <s2siddhu@tamu.edu>
2020-12-19 01:01:30 -0800

    RC working and tested, chassis control code implemented
    ^
    Notes:
    pog
    - Sid Prabhakaran
    12/19/20

--- Core/Src/subsystems/chassis.cpp
+++ Core/Src/subsystems/chassis.cpp
@@ -6,10 +6,22 @@
 #include "init.hpp"
 #include <arm_math.h>
 
-int16_t rightX;
-int16_t rightY;
-int16_t leftX;
-int16_t leftY;
+float rightX;
+float rightY;
+float leftX;
+float leftY;
+float switch1;
+float switch2;
+float angle;
+float angleOutput;
+float magnitude;
+float c1Output;
+float turning;
+float disp;
+float motor1P;
+float motor2P;
+float motor3P;
+float motor4P;
 
 namespace chassis {
 
@@ -17,12 +29,15 @@ chassisStates currState = notRunning;
 CtrlTypes ctrlType = CURRENT;
 // i don't really like this but do i care enough to change it?
 
-pidInstance velPid(pidType::velocity, 0.7, 0.0, 0.0);
-
-chassisMotor c1Motor(userCAN::M3508_M1_ID, velPid);
-chassisMotor c2Motor(userCAN::M3508_M2_ID, velPid);
-chassisMotor c3Motor(userCAN::M3508_M3_ID, velPid);
-chassisMotor c4Motor(userCAN::M3508_M4_ID, velPid);
+pidInstance velPidC1(pidType::velocity, 0.7, 0.0, 0.0);
+pidInstance velPidC2(pidType::velocity, 0.7, 0.0, 0.0);
+pidInstance velPidC3(pidType::velocity, 0.7, 0.0, 0.0);
+pidInstance velPidC4(pidType::velocity, 0.7, 0.0, 0.0);
+	
+chassisMotor c1Motor(userCAN::M3508_M1_ID, velPidC1);
+chassisMotor c2Motor(userCAN::M3508_M2_ID, velPidC2);
+chassisMotor c3Motor(userCAN::M3508_M3_ID, velPidC3);
+chassisMotor c4Motor(userCAN::M3508_M4_ID, velPidC4);
 
 void task() {
 
@@ -38,19 +53,29 @@ void task() {
 
 void update() {
     if (true) {
-        currState = notRunning;
+        currState = manual;
         // will change later based on RC input and sensor based decision making
     }
     
-    rightX = (rcDataStruct.rc.ch[0]);
-    rightY = (rcDataStruct.rc.ch[1]);
-    leftX = (rcDataStruct.rc.ch[2]);
-    leftY = (rcDataStruct.rc.ch[3]);
+    rightX = static_cast<float>(rcDataStruct.rc.ch[0]) / 660;
+    rightY = static_cast<float>(rcDataStruct.rc.ch[1]) / 660;
+    leftX = static_cast<float>(rcDataStruct.rc.ch[2]) / 660;
+    leftY = static_cast<float>(rcDataStruct.rc.ch[3]) / 660;
+		
+		switch1 = (rcDataStruct.rc.s[0]);
+		switch2 = (rcDataStruct.rc.s[1]);
 
-		//float rcSomething = leftX;
-		float rcSomething = /*(abs(leftX) <= 30) ? 0 :*/ leftX;
-
-    rcSomething = rcSomething / 1320 * 100;
+		
+		angle = atan2(leftY, leftX);
+		angleOutput = angle / PI * 180;
+		
+		magnitude = sqrt(pow(leftY, 2) + pow(leftX, 2));
+		
+		
+		
+		
+		c1Output = static_cast<double>(c1Motor.getFeedback()->rotor_speed) / 19.0;
+		
     // divide by total range and scale to -100, 100
 
 		//velPidC1.setTarget(rcSomething); // back left
@@ -78,25 +103,58 @@ void act() {
         break;
 
     case manual:
-        double power = velPid.loop(static_cast<double>(c1Motor.getFeedback()->rotor_speed) / 19.0);
-        c1Motor.setPower(power);
-        c2Motor.setPower(power);
-        c3Motor.setPower(power);
-        c4Motor.setPower(power);
+				rcToPower(angle, magnitude, rightX);
+        double power1 = velPidC1.loop(static_cast<double>(c1Motor.getFeedback()->rotor_speed) / 19.0);
+				double power2 = velPidC2.loop(static_cast<double>(c2Motor.getFeedback()->rotor_speed) / 19.0);
+				double power3 = velPidC3.loop(static_cast<double>(c3Motor.getFeedback()->rotor_speed) / 19.0);
+				double power4 = velPidC4.loop(static_cast<double>(c4Motor.getFeedback()->rotor_speed) / 19.0);
+				c1Motor.setPower(power1);
+        c2Motor.setPower(power2);
+        c3Motor.setPower(power3);
+        c4Motor.setPower(power4);
         // if current control, power will be set in the CAN task
         // this will change when we have things to put here
         break;
     }
 }
 
-void rcToPower(double angle, double magnitude) {
+void rcToPower(double angle, double magnitude, double yaw) {
     // Computes the appropriate fraction of the wheel's motor power
 
     // Sine and cosine of math.h take angle in radians as input value
-    c1Motor.setPower(magnitude * sqrt(2.0) * 0.5 * (cos(angle) + sin(angle))); // 
-    c2Motor.setPower(magnitude * sqrt(2.0) * 0.5 * (sin(angle) - cos(angle)));
-    c3Motor.setPower(magnitude * sqrt(2.0) * 0.5 * (sin(angle) - cos(angle)));
-    c4Motor.setPower(magnitude * sqrt(2.0) * 0.5 * (cos(angle) + sin(angle)));
+		// motor 1 back left
+		// motor 2 front left
+		// motor 3 front right
+		// motor 4 back right
+		float turnScalar = 0.6;
+	
+		disp = ((abs(magnitude) + abs(yaw)) == 0) ? 0 : abs(magnitude) / (abs(magnitude) + turnScalar*abs(yaw));
+		turning = ((abs(magnitude) + abs(yaw)) == 0) ? 0 : turnScalar*abs(yaw) / (abs(magnitude) + turnScalar*abs(yaw));
+		//disp = 1 - abs(turning);
+		// disp and turning represent the percentage of how much the user wants to displace or turn
+		// displacement takes priority here
+	
+		float motor1Turn = yaw;
+		float motor2Turn = yaw;
+		float motor3Turn = yaw;
+		float motor4Turn = yaw;
+	
+		float motor1Disp = (magnitude * (sin(angle) - cos(angle))) * 1;
+		float motor2Disp = (magnitude * (cos(angle) + sin(angle))) * 1;
+		float motor3Disp = (magnitude * (sin(angle) - cos(angle))) * -1;
+		float motor4Disp = (magnitude * (cos(angle) + sin(angle))) * -1;
+	
+		motor1P = (turning * motor1Turn) + (disp * motor1Disp);
+		motor2P = (turning * motor2Turn) + (disp * motor2Disp);
+		motor3P = (turning * motor3Turn) + (disp * motor3Disp);
+		motor4P = (turning * motor4Turn) + (disp * motor4Disp);
+	
+    velPidC1.setTarget(motor1P * 200);
+    velPidC2.setTarget(motor2P * 200);
+    velPidC3.setTarget(motor3P * 200);
+    velPidC4.setTarget(motor4P * 200);
+		// scaling speed up to 200 rpm, can be set up to 482rpm
+		
 }
 
 } // namespace chassis
